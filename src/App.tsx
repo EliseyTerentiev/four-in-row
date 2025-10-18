88e8e8620c0d549f7e5f114d8600ba5d04b7c36c
EliseyTerentiev <elis.228723@gmail.com>
2025-10-18 15:33:02 +0500

    tailwind and prettier added

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a
+          href="https:/npm install tailwindcss @tailwindcss/vite/vite.dev"
+          target="_blank"
+        >
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -21,7 +24,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="font-bold text-red-300">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
